9868504b25e7b0a744afaefe32eba288814bd6ab
truong <truong992001@gmail.com>
2022-03-25 14:50:35 +0700

    ThucHanhJava_Thymeleaf_Spring_Devtool

---
+++ SpringBootMeleaf/src/main/java/springboot_hello/SpringBootMeleafApplication.java
@@ -0,0 +1,13 @@
+package springboot_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMeleafApplication.class, args);
+	}
+
+}
